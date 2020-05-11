7341d67b749c3a6c7202040bfcca1f3bd77d9c93
Watsapon 'Ty' Kampong <51149687+watsaponk@users.noreply.github.com>
2020-05-11 19:00:53 +0700

    feat: add navigation component (#1)
    * build: add navigation component dependency
    
    * feat(lesson): add fragment
    
    * feat: change app name

---
+++ app/src/main/java/com/app/kaidee/dojo/lesson/LessonFragment.kt
@@ -0,0 +1,24 @@
+package com.app.kaidee.dojo.lesson
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.app.kaidee.dojo.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LessonFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lesson, container, false)
+    }
+
+}
